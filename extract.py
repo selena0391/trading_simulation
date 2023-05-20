0712a3bedec222b46e788e028430a2cf29158e8b
Ezequiel Esparza <ezequiel.esparza1998@gmail.com>
2023-05-20 16:14:31 -0400

    linear regression simulation/calculator

--- extract.py
+++ extract.py
@@ -9,3 +9,12 @@ snp500 = yf.download('^GSPC', start=start_date, end=end_date)
 
 # Save the data to a CSV file
 snp500.to_csv('s&p500_data.csv')
+
+
+start_date = '2000-01-01'
+end_date = '2023-05-19'
+
+# Fetch the CBOE Volatility Index (^VIX) data
+vix = yf.download('^VIX', start=start_date, end=end_date)
+# Save the data to a CSV file
+vix.to_csv('vix_data.csv')
--- linear_app.py
+++ linear_app.py
@@ -1,65 +1,45 @@
 from flask import Flask, render_template, request
-import csv
-from datetime import datetime
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
-@app.route('/')
+df = pd.read_csv('data/s&p500_data.csv')
+df['Date'] = pd.to_datetime(df['Date'])
+df['Year'] = df['Date'].dt.year
+
+X = df['Year'].values.reshape(-1, 1).astype(float)
+y = df['Open']
+
+model = LinearRegression()
+model.fit(X, y)
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('linear_index.html')
 
-@app.route('/simulate', methods=['POST'])
-def simulate():
-    start_date = datetime.strptime(request.form['start_date'], "%Y-%m-%d").date()
-    end_date = datetime.strptime(request.form['end_date'], "%Y-%m-%d").date()
-    money_invested = float(request.form['money_invested'])
+@app.route('/calculate_growth', methods=['POST'])
+def calculate_growth():
+    initial_investment = float(request.form['investment'])
+    current_year = df['Year'].max()
+    future_years = range(current_year + 1, current_year + 11)  # Predicting for the next 10 years
+    future_X = pd.DataFrame(future_years, columns=['Year'])
+    future_y_pred = model.predict(future_X)
 
-    # Read the CSV file
-    data = []
-    with open('data/s&p500_data.csv', 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            date = datetime.strptime(row['Date'], "%Y-%m-%d").date()
-            if start_date <= date <= end_date:
-                data.append({
-                    'date': date,
-                    'open': float(row['Open']),
-                    'close': float(row['Close'])
-                })
+    future_dates = pd.date_range(start='2023-05-01', periods=10, freq='AS')
+    
+    current_price = df.loc[df['Year'] == current_year, 'Open'].values[0]
+    growth_rates = future_y_pred / current_price
 
-    # Perform the simulation trading logic
-    portfolio_value = money_invested
-    trades = 0
-    for i in range(len(data) - 1):
-        current_price = data[i]['close']
-        next_price = data[i + 1]['open']
-        if next_price > current_price:
-            # Buy
-            shares_to_buy = portfolio_value / current_price
-            portfolio_value = shares_to_buy * next_price
-            trades += 1
+    future_values = [initial_investment * (1 + growth) for growth in growth_rates]
 
-    # Convert portfolio value to a dollar amount with 2 decimal places
-    portfolio_value = round(portfolio_value, 2)
+    result = [{'date': date.strftime('%Y-%m-%d'), 'value': value} for date, value in zip(future_dates, future_values)]
+    return render_template('linear_result.html', result=result)
 
-    # Calculate the percentage change
-    percent_change = ((portfolio_value - money_invested) / money_invested) * 100
 
-    # Calculate the current value
-    current_value = portfolio_value
-
-    # Construct the simulation result
-    simulation_result = {
-        'start_date': start_date,
-        'end_date': end_date,
-        'money_invested': money_invested,
-        'portfolio_value': portfolio_value,
-        'trades': trades,
-        'percent_change': percent_change,
-        'current_value': current_value
-    }
-
-    return render_template('result.html', result=simulation_result)
+@app.route('/result', methods=['POST'])
+def result():
+    return render_template('linear_result.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
